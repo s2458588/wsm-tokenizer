1a9ecef7a27b46b88b238ab06a9c5dc7f4c8c49b
s2458588 <s2458588@stud.uni-frankfurt.de>
2023-01-29 15:14:39 +0100

    Refactor: tokenizer.ipynb -> wm_tokenizer.ipynb
    Implemented:
    wordmap tokenizer maximum weight segmentation

--- src/wm_tokenizer.py
+++ src/wm_tokenizer.py
@@ -15,43 +15,52 @@ import numpy as np
 class SequenceTokenizer:
     """Generate a series of possible segmentations from a given vocabulary (dict: {string:relative freqency}) for a
     given target token."""
-    segment_stack = []
+
+    # segment_stack = []
 
     def __init__(self, vocab: dict, target: str):
         self.vocab = vocab
         self.target = target
         self.subvocab = [i for i in self.vocab if i in self.target]
-        self.run = self.segmenter(self.target, stop=len(target))
-        self.maxed = self.maximize_segments(vocab=self.vocab, segmentations=self.segment_stack)
+        self.segmentations = []
+        self.segmenter(self.target, stop=len(target))
+        self.maxed = None
+        self.maximize_segments()
+        print(self.segmentations, self.maxed)
 
-    def segmenter(self, token, segments=None, start=0, stop=int):
+    def fill_stack(self, s):
+        self.segmentations.append(s)
 
+    def segmenter(self, token, stop: int, start=0, segments=None, stack=None):
+        if stack is None:
+            stack = []
         if segments is None:
             segments = []
         if start == stop:
-            self.segment_stack.append(segments)
+            stack.append(segments)
+            self.segmentations.append(segments)
+            #self.fill_stack(segments)
 
-            return segments
         else:
             new_morpheme = [i for i in self.subvocab if token.startswith(i) and len(i) > 1]
             for m in new_morpheme:
                 rest = token[len(m):]
                 start += len(m)
                 segments.append(m)
-                self.segmenter(token=rest, segments=segments.copy(), start=start, stop=stop)
+                self.segmenter(token=rest, stop=stop, start=start, segments=segments.copy(), stack=stack.copy())
                 segments = segments[:-1]
                 start -= len(m)
 
-    def maximize_segments(self, vocab, segmentations):
+    def maximize_segments(self):
         ws = dict()
-        len_tk = len("".join(segmentations[0]))
-        for s in segmentations:
+        len_tk = len("".join(self.segmentations[0]))
+        for s in self.segmentations:
             coverage = [len(i) / len_tk for i in s]  # how much % of the word is covered by this morpheme
             morpheme_length = [len(i) for i in s]  # how long is each morpheme
-            rel_freq = [vocab[i] for i in s]  # relative frequency of the morpheme in the vocab
+            rel_freq = [self.vocab[i] for i in s]  # relative frequency of the morpheme in the vocab
             n_o_segs = [len(s) for i in s]
 
             lex_bias = sum([(np.tanh(mlen) / nsegs) for mlen, nsegs, freq in zip(morpheme_length, n_o_segs, rel_freq)])
 
             ws[lex_bias] = s
-        return ws[max(ws.keys())]
+        self.maxed = ws[max(ws.keys())]
